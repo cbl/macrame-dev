5958c75068cbcd6420f9c889a1d79cfc2c8d4497
Chrissle28 <christian.z.dev@gmail.com>
2021-06-30 16:21:52 +0200

    frontend toggle vue

--- resources/js/vue3/components/Toggle.vue
+++ resources/js/vue3/components/Toggle.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="p-24">
         <div class="flex mb-5 space-x-5">
-            <ui-toggle yellow sm> Foo </ui-toggle>
-            <ui-toggle green md> Bar </ui-toggle>
-            <ui-toggle blue lg> Baz </ui-toggle>
+            <ui-toggle v-model="toggle" yellow sm> Foo </ui-toggle>
+            <ui-toggle v-model="toggle" green md> Bar </ui-toggle>
+            <ui-toggle v-model="toggle" blue lg> Baz </ui-toggle>
         </div>
         <div class="flex mb-5 space-x-5">
-            <ui-toggle variant="red" size="sm" label="Foo" />
-            <ui-toggle size="md" label="Bar" />
-            <ui-toggle size="lg" label="Baz" />
+            <ui-toggle v-model="toggle" variant="red" size="sm" label="Foo" />
+            <ui-toggle v-model="toggle" size="md" label="Bar" />
+            <ui-toggle v-model="toggle" size="lg" label="Baz" />
         </div>
+        {{ toggle }}
     </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 export default defineComponent({
     setup() {
-        console.log('hi');
+        const toggle = ref(false);
+        return {
+            toggle,
+        };
     },
 });
 </script>
