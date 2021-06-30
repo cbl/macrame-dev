8a6c97c1f3ec5be82fcd899db63fd8fd9b38643a
Chrissle28 <christian.z.dev@gmail.com>
2021-06-30 13:56:05 +0200

    more margin on toggle

--- resources/js/vue3/components/Toggle.vue
+++ resources/js/vue3/components/Toggle.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="p-24">
-        <div class="flex mb-4 space-x-5">
+        <div class="flex mb-5 space-x-5">
             <ui-toggle sm> Foo </ui-toggle>
             <ui-toggle md> Bar </ui-toggle>
             <ui-toggle lg> Baz </ui-toggle>
         </div>
-        <div class="flex mb-4 space-x-5">
+        <div class="flex mb-5 space-x-5">
             <ui-toggle size="sm" label="Foo" />
             <ui-toggle size="md" label="Bar" />
             <ui-toggle size="lg" label="Baz" />
