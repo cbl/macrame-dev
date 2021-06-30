ad1ed8ad2ac5874a23b07cd7a9a10449a643bd6e
Chrissle28 <christian.z.dev@gmail.com>
2021-06-30 13:48:28 +0200

    moved mode: jit to sub module admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const merge = require('lodash.merge');
 const tailwindConfig = require('@macramejs/admin-vue3/tailwind.config');
 
 module.exports = merge(tailwindConfig, {
-    mode: 'jit',
     purge: [
         './resources/js/**/*.vue',
         './packages/admin/packages/admin-vue3/src/ui/**/*.vue',
